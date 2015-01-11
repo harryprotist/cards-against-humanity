97f01892d23afd9051b843cbd0679bb2332a850f
Ben Sharafian <benms64@gmail.com>
2015-01-10 18:13:23 -0800

    fixes CardGame.sh, again

--- CardGame.sh
+++ CardGame.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-python2 $DIR/resources/CardGame.py $1 $2 $3
+(
+cd $DIR/resources;
+python2 CardGame.py $1 $2 $3
+) # makes sure that program doesn't exit in 'resources'
